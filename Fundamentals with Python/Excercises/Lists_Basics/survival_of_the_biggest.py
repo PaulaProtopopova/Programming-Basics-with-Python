bb82c8f0f87b552ad516ff6197c05ed8303b21df
Paula Protopopova <paula.milenova@gmail.com>
2023-05-31 22:54:45 +0300

    -m

--- Fundamentals with Python/Excercises/Lists_Basics/survival_of_the_biggest.py
+++ Fundamentals with Python/Excercises/Lists_Basics/survival_of_the_biggest.py
@@ -1,5 +1,11 @@
 list_of_nums = input().split()
-number_as_digit = input().sorted().
+numeric_list = []
+for element in list_of_nums:
+    number = int(element)
+    numeric_list.append(number)
+remover = int(input())
+for _ in range(remover):
+    numeric_list.remove(min(numeric_list))
+print(numeric_list)
 
-print(list_of_nums)
 
