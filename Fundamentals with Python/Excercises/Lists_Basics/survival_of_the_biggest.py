a326dfb477ae58e19352c8d31f8d865283e4a52f
Paula Protopopova <paula.milenova@gmail.com>
2023-06-01 15:23:13 +0300

    -m

--- Fundamentals with Python/Excercises/Lists_Basics/survival_of_the_biggest.py
+++ Fundamentals with Python/Excercises/Lists_Basics/survival_of_the_biggest.py
@@ -1,11 +1,23 @@
-list_of_nums = input().split()
-numeric_list = []
-for element in list_of_nums:
-    number = int(element)
-    numeric_list.append(number)
+#CONDITION OF TASK:
+# Write a program that receives a list of integer numbers (separated by a single space)
+# and a number n. The number n represents the count of numbers to remove from the list.
+# should remove the smallest ones, and then, you should print all the numbers that are left in the list, separated by a comma and a space ", ".
+
+
+#FIRST OPTION
+# list_of_nums = input().split()
+# numeric_list = []
+# for element in list_of_nums:
+#     number = int(element)
+#     numeric_list.append(number)
+# remover = int(input())
+# for _ in range(remover):
+#     numeric_list.remove(min(numeric_list))
+# print(*numeric_list, sep=", ")
+
+#OPTIMIZED
+numbers = list(map(int, input().split()))
 remover = int(input())
 for _ in range(remover):
-    numeric_list.remove(min(numeric_list))
-print(*numeric_list, sep=", ")
-
-
+    numbers.remove(min(numbers))
+print(*numbers, end=" ")
