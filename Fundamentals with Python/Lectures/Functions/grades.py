0dd2c7a5bfeee484953131f07a76eeb8890edabc
Paula Protopopova <paula.milenova@gmail.com>
2023-06-02 00:27:24 +0300

    -m

--- Fundamentals with Python/Lectures/Functions/grades.py
+++ Fundamentals with Python/Lectures/Functions/grades.py
@@ -1,4 +1,25 @@
-def greet(name):
-    return f'Hello, {name}!'
+# ------------------------------CONDITION OF TASK-------------------------------
+# Write a function that receives a grade between 2.00 and 6.00 and prints the corresponding grade in words.
+# • 2.00 – 2.99 - "Fail"
+# • 3.00 – 3.49 - "Poor"
+# • 3.50 – 4.49 - "Good"
+# • 4.50 – 5.49 - "Very Good"
+# • 5.50 – 6.00 - "Excellent"
 
-print(greet('PAffka'))
+grade = float(input())
+
+def what_is_my_grade():
+    if 2.00 <= grade <= 2.99:
+        return 'Fail'
+    elif 3.00 <= grade <= 3.49:
+        return 'Poor'
+    elif 3.50 <= grade <= 4.49:
+        return 'Good'
+    elif 4.50 <= grade <= 5.49:
+        return 'Very Good'
+    elif 5.50 <= grade <= 6.00:
+        return 'Excellent'
+
+print(what_is_my_grade())
+
+
