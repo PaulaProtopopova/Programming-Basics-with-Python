10cb0625b664a88d7a063f30dab9962f5190fde4
Paula Protopopova <paula.milenova@gmail.com>
2023-06-02 18:01:13 +0300

    -m

--- Fundamentals with Python/Lectures/Lists Basics/numbers_filter.py
+++ Fundamentals with Python/Lectures/Lists Basics/numbers_filter.py
@@ -1,3 +1,13 @@
+# ------------------------------CONDITION OF THE TASK-------------------------------------
+# On the first line, you will receive a single number n. On the following n lines,
+# you will receive integers. After that, you will be given one of the following commands:
+# •even
+# •odd
+# •negative
+# •positive
+# Filter all the numbers that fit in the category (0 counts as a positive and even).
+# Finally, print the result.
+
 n = int(input())
 filtered_lst = []
 initial_list = []
@@ -20,7 +30,7 @@ elif command == 'negative':
             filtered_lst.append(number)
 elif command == 'positive':
     for number in initial_list:
-        if number > 0:
+        if number >= 0:
             filtered_lst.append(number)
 
 print(filtered_lst)
