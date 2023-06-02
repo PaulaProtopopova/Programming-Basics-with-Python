f2fac790ed5877e7e810cc130e9cc914ee620a43
Paula Protopopova <paula.milenova@gmail.com>
2023-06-02 17:57:46 +0300

    -m

--- Fundamentals with Python/Lectures/Lists Basics/numbers_filter.py
+++ Fundamentals with Python/Lectures/Lists Basics/numbers_filter.py
@@ -1,7 +1,27 @@
 n = int(input())
-lst = []
+filtered_lst = []
+initial_list = []
 for _ in range(n):
-    num = int(input())
-
+    number = int(input())
+    initial_list.append(number)
 command = input()
-if command == 'even' and num % 2 == 0:
+
+if command == 'even':
+    for number in initial_list:
+        if number % 2 == 0:
+            filtered_lst.append(number)
+elif command == 'odd':
+    for number in initial_list:
+        if number % 2 != 0:
+            filtered_lst.append(number)
+elif command == 'negative':
+    for number in initial_list:
+        if number < 0:
+            filtered_lst.append(number)
+elif command == 'positive':
+    for number in initial_list:
+        if number > 0:
+            filtered_lst.append(number)
+
+print(filtered_lst)
+
