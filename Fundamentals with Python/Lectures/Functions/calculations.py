09f7980f91c0b386e0671e5e804c6a9e8a0ec5b2
Paula Protopopova <paula.milenova@gmail.com>
2023-06-03 16:23:32 +0300

    -m

--- Fundamentals with Python/Lectures/Functions/calculations.py
+++ Fundamentals with Python/Lectures/Functions/calculations.py
@@ -4,26 +4,43 @@
 # The input comes as three parameters – an operator as a string and two integer numbers.
 # The operator can be one of the following: "multiply", "divide", "add", "subtract".
 
+#------------------------OPTION 1-----------------------------------
+
+# def calculations():
+#     operator = input()
+#     num_1 = int(input())
+#     num_2 = int(input())
+#     result = 0
+#     if operator == 'multiply':
+#         result = num_1 * num_2
+#         return result
+#     elif operator == 'divide':
+#         if num_2 != 0:
+#             result = num_1 / num_2
+#             return f'{result:.0f}'
+#     elif operator == 'add':
+#         result = num_1 + num_2
+#         return result
+#     elif operator == 'subtract':
+#         result = num_1 - num_2
+#         return result
+#
+#
+# print(calculations())
+
+#---------------------------OPTIMIZED-------------------------------
+
+def calculations(operator, num1, num2):
+    return {
+        'multiply': num1 * num2,
+        'divide': int(num1/num2),
+        'add': num1 + num2,
+        'subtract': num1 - num2
+    }.get(operator, 'Invalid operator')
 
 
-def calculations():
-    operator = input()
-    num_1 = int(input())
-    num_2 = int(input())
-    result = 0
-    if operator == 'multiply':
-        result = num_1 * num_2
-        return result
-    elif operator == 'divide':
-        if num_2 != 0:
-            result = num_1 / num_2
-            return f'{result:.0f}'
-    elif operator == 'add':
-        result = num_1 + num_2
-        return result
-    elif operator == 'subtract':
-        result = num_1 - num_2
-        return result
+operator = input()
+num1 = int(input())
+num2 = int(input())
 
-
-print(calculations())
+print(calculations(operator, num1, num2))
